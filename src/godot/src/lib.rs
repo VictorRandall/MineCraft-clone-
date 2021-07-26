e518338ca3d3893678144a65a17f61334c085ff5
Victor Randall <victor.randall13@gmail.com>
2021-07-26 15:08:10 -0300

    tried to use add_triangle_fan but i suck at code

--- src/godot/src/lib.rs
+++ src/godot/src/lib.rs
@@ -1,67 +1,90 @@
-use gdnative::api::{ArrayMesh, Mesh, MeshInstance, OpenSimplexNoise, SurfaceTool, Spatial};
+use gdnative::api::{ArrayMesh, Mesh, MeshInstance, OpenSimplexNoise, SurfaceTool, Spatial, StaticBody};
 use gdnative::prelude::*;
 
 #[derive(NativeClass)]
-#[inherit(Node)]
+#[inherit(StaticBody)]
+#[register_with(Self::register_properties)]
+//#[inherit(StaticBody)]
 //mod voxelchunk;
 
-//pub struct VoxelChunk {
-//	position:[f32, 3],
-//	data:[u16,4096]
-//}
+pub struct VoxelChunk {
+	position:f32,
+	data:f32,
+}
+
+#[methods]
+impl VoxelChunk {
+	fn new(_owner: &StaticBody) -> Self {
+		VoxelChunk{
+			position: 1.0,
+			data: 1.0,
+		}
+	}
+} 
+
+#[derive(NativeClass)]
+#[inherit(Spatial)]
+#[register_with(Self::register_properties)]
+
 pub struct VoxelWorld{
 	size_x:i32,
 	size_y:i32,
 	size_z:i32,
 }
-//
-//
-//impl VoxelChunk {
-//	fn new(_owner: &StaticBody) -> Self {
-//		VoxelChunk
-//	}
-//} 
 
 #[methods]
 impl VoxelWorld {
-	fn new(_owner: &Node) -> Self {
+	fn new(_owner: &Spatial) -> Self {
 		VoxelWorld{
-			size_x: 16,
-			size_y: 16,
-			size_z: 16,
+			size_x: 1,
+			size_y: 1,
+			size_z: 1,
 		}
 	}
 
 
 	#[export]
-	fn _ready(&self, _owner: &Node) {
+	fn _ready(&self, _owner: &Spatial) {
 		godot_print!("_ready (rust)");
 	}
 	#[export]
-	fn _process(&mut self, owner: &Node, _delta: f64){
+	fn _process(&mut self, owner: &Spatial, _delta: f64){
 		let input = Input::godot_singleton();
 		let st = SurfaceTool::new();
-		let mut array = TypedArray::new();
+//		let mut array = TypedArray::new();
 //		st.begin(Mesh::PRIMITIVE_TRIANGLES);
 		st.begin(Mesh::PRIMITIVE_LINES);
 //		st.add_smooth_group(true);
 //		
-//		if input.is_action_just_pressed("test"){
-//			self.size_x = 3
-//		}else if input.is_action_just_pressed("test2"){
-//			self.size_x = 6
-//		}
-		
-		for x in self.size_x - (self.size_x * 2)..self.size_x / 2{
-			for y in self.size_y - (self.size_y * 2)..self.size_y / 2{
-				for z in self.size_z - (self.size_z * 2)..self.size_z / 2{
-					cube(&mut array,&st, Vector3::new(x as f32,y as f32,z as f32));
-				}
-			}
+		if input.is_action_just_pressed("test"){
+			self.size_x += 1;
+			self.size_y += 1;
+			self.size_z += 1;
+		}else if input.is_action_just_pressed("test2"){
+			self.size_x -= 1;
+			self.size_y -= 1;
+			self.size_z -= 1;
 		}
+		
+
+		//this block was commented to experiment add_triangle_fan in line 81
+//		for x in 0..self.size_x{
+//			for y in 0..self.size_y{
+//				for z in 0..self.size_z{
+//					cube(&mut array,&st, Vector3::new(x as f32,y as f32,z as f32));
+//					godot_print!("created block number Vector3({},{},{})!", x,y,z)
+//				}
+//			}
+//		}
+
+		
+
 		st.generate_normals(false);
 		let mesh: Ref<ArrayMesh> = st.commit(gdnative::Null::null(), Mesh::ARRAY_COMPRESS_DEFAULT).unwrap();
-//		unsafe {owner.get_node("MeshInstance").unwrap().assume_safe().cast::<MeshInstance>().unwrap().set_mesh(mesh)};
+		godot_print!("commited mesh!");
+		unsafe {
+			owner.get_node("MeshInstance").unwrap().assume_safe().cast::<MeshInstance>().unwrap().set_mesh(mesh)
+		};
 	}
 
 }
@@ -80,7 +103,7 @@ fn cube(data:&mut gdnative::core_types::TypedArray<i32>,st:&Ref<SurfaceTool, Uni
 	let modi_uv_x:f32 = 0.0;
 
 	data.insert(0,1);
-
+//	godot_print!("{}",data);
 
 
 	//top
@@ -182,7 +205,15 @@ godot_init!(init);
 //}
 //?: como juntar/transformar duas strings/int/float/etc em uma so string
 //format!()
-
+//try using add_triangle_fan
+//	st.add_triangle_fan(
+//		TypedArray::<Vector3>(Vector3::new(0.0,1.0,0.0),Vector3::new(0.0,1.0,0.0),Vector3::new(0.0,1.0,1.0)),
+//		TypedArray::<Vector2>(Vector2::new(0.0,0.0),Vector2::new(1.0,0.0),Vector2::new(1.0,1.0)),
+//		gdnative::Null::null(),
+//		gdnative::Null::null(),
+//		gdnative::Null::null(),
+//		gdnative::Null::null()
+//	);
 
 
 
