d5aa45d7693f662a50791e512888033ed734b454
Victor Randall <victor.randall13@gmail.com>
2021-07-27 18:25:12 -0300

    everything glitched, idk what hapened

--- src/godot/target/debug/deps/godot.d
+++
@@ -1,5 +0,0 @@
-/home/victorrandall/projs/MineCraft-clone-/src/godot/target/debug/deps/libgodot.so: src/lib.rs
-
-/home/victorrandall/projs/MineCraft-clone-/src/godot/target/debug/deps/godot.d: src/lib.rs
-
-src/lib.rs:
--- src/godot/target/debug/libgodot.d
+++
@@ -1,1 +0,0 @@
-/home/victorrandall/projs/MineCraft-clone-/src/godot/target/debug/libgodot.so: /home/victorrandall/projs/MineCraft-clone-/src/godot/src/lib.rs
