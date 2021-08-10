ae6d409bcbf73365333e4154e669b4312665b838
Victor Randall <victor.randall13@gmail.com>
2021-08-10 18:28:34 -0300

    no change of status

--- src/VoxelTool/src/voxel_chunk/voxel.rs
+++ src/VoxelTool/src/voxel_chunk/voxel.rs
@@ -4,19 +4,19 @@ use gdnative::prelude::*;
 pub struct Voxel<T,Y,R,S>{
 	solid:bool,
 	id:u16,
-	mesh:Vec<Option<ArrayMesh>>,
+//	mesh:Vec<Option<gdnative::Ref<ArrayMesh, gdnative::prelude::Unique>>>,
 	parameters:Option<Vec<T>>,
 	parameters2:Option<Vec<Y>>,
 	parameters3:Option<Vec<R>>,
 	parameters4:Option<Vec<S>>,
 }
 
-impl<T,Y,R,S> Voxel<T,Y,R,S>>{
-	pub fn new(sld:bool, id:u16,prmtrs:Option<Vec<T>>,prmtrs2:Option<Vec<Y>>,prmtrs3:Option<Vec<T>>,prmtrs4:Option<Vec<T>>) -> Self{
-		Voxel{
+impl<T,Y,R,S> Voxel<T,Y,R,S>{
+	pub fn new(sld:bool, id:u16,prmtrs:Option<Vec<T>>,prmtrs2:Option<Vec<Y>>,prmtrs3:Option<Vec<R>>,prmtrs4:Option<Vec<S>>) -> Self{
+		return Voxel{
 			solid:sld,
 			id:id,
-			mesh:Some(),
+//			mesh:vec![Some(gdnative::Ref<ArrayMesh, gdnative::prelude::Unique>]),
 			parameters:prmtrs,
 			parameters2:prmtrs2,
 			parameters3:prmtrs3,
