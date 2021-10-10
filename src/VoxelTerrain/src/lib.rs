69edd07a69cfc022bd462e30603f6c9f7e50c8b4
Victor Randall <victor.randall13@gmail.com>
2021-10-10 18:32:16 -0300

    the set_voxel function works now

--- src/VoxelTerrain/src/lib.rs
+++ src/VoxelTerrain/src/lib.rs
@@ -25,129 +25,6 @@ pub struct HelloWorld{
 impl HelloWorld {
     fn new(_owner: &Spatial) -> Self {
         HelloWorld{
-//        	block_types: vec![
-//								vec![ //grass
-//		    							vec![//top
-//											Vertex{uv: Vector2::new(1.0, 0.0),vertex: Vector3::new(0.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(2.0, 0.0),vertex: Vector3::new(1.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(1.0, 1.0),vertex: Vector3::new(0.0,1.0,1.0)},
-//											
-//											Vertex{uv: Vector2::new(1.0, 1.0),vertex: Vector3::new(0.0,1.0,1.0)},
-//											Vertex{uv: Vector2::new(2.0, 0.0),vertex: Vector3::new(1.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(2.0, 1.0),vertex: Vector3::new(1.0,1.0,1.0)}
-//		    							],
-//		    							
-//		    							vec![//botton
-//		    								Vertex{uv: Vector2::new(2.0, 0.0),vertex: Vector3::new(0.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(3.0, 1.0),vertex: Vector3::new(1.0,0.0,0.0)},
-//		    								Vertex{uv: Vector2::new(2.0, 1.0),vertex: Vector3::new(0.0,0.0,0.0)},
-//		    								
-//		    								Vertex{uv: Vector2::new(3.0, 0.0),vertex: Vector3::new(1.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(3.0, 1.0),vertex: Vector3::new(1.0,0.0,0.0)},
-//		    								Vertex{uv: Vector2::new(2.0, 0.0),vertex: Vector3::new(0.0,0.0,1.0)}
-//		    							],
-//		    							
-//		    							vec![//right
-//		    								Vertex{uv: Vector2::new(1.0, 1.0), vertex: Vector3::new(1.0,0.0,0.0)},
-//		    								Vertex{uv: Vector2::new(0.0, 1.0), vertex: Vector3::new(1.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(1.0, 0.0), vertex: Vector3::new(1.0,1.0,0.0)},
-//		    								
-//		    								Vertex{uv: Vector2::new(0.0, 1.0), vertex: Vector3::new(1.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(0.0, 0.0), vertex: Vector3::new(1.0,1.0,1.0)},
-//		    								Vertex{uv: Vector2::new(1.0, 0.0), vertex: Vector3::new(1.0,1.0,0.0)}
-//		    							],
-//		    							
-//		    							vec![//left
-//		    								Vertex{uv: Vector2::new(0.0, 0.0), vertex: Vector3::new(0.0,1.0,0.0)},
-//		    								Vertex{uv: Vector2::new(1.0, 1.0), vertex: Vector3::new(0.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(0.0, 1.0), vertex: Vector3::new(0.0,0.0,0.0)},
-//		    								
-//		    								Vertex{uv: Vector2::new(1.0, 0.0), vertex: Vector3::new(0.0,1.0,1.0)},
-//		    								Vertex{uv: Vector2::new(1.0, 1.0), vertex: Vector3::new(0.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(0.0, 0.0), vertex: Vector3::new(0.0,1.0,0.0)}
-//		    							],
-//		    							
-//		    							vec![//back
-//											Vertex{uv: Vector2::new(0.0,1.0), vertex: Vector3::new(0.0,0.0,1.0)},
-//											Vertex{uv: Vector2::new(0.0,0.0), vertex: Vector3::new(0.0,1.0,1.0)},
-//											Vertex{uv: Vector2::new(1.0,1.0), vertex: Vector3::new(1.0,0.0,1.0)},
-//											
-//											Vertex{uv: Vector2::new(1.0,1.0), vertex: Vector3::new(1.0,0.0,1.0)},
-//											Vertex{uv: Vector2::new(0.0,0.0), vertex: Vector3::new(0.0,1.0,1.0)},
-//											Vertex{uv: Vector2::new(1.0,0.0), vertex: Vector3::new(1.0,1.0,1.0)},
-//		    							],
-//		    							
-//		    							vec![//front
-//											Vertex{uv: Vector2::new(0.0,1.0), vertex: Vector3::new(1.0,0.0,0.0)},
-//											Vertex{uv: Vector2::new(1.0,0.0), vertex: Vector3::new(0.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(1.0,1.0), vertex: Vector3::new(0.0,0.0,0.0)},
-//											
-//											Vertex{uv: Vector2::new(0.0,0.0), vertex: Vector3::new(1.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(1.0,0.0), vertex: Vector3::new(0.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(0.0,1.0), vertex: Vector3::new(1.0,0.0,0.0)},
-//		    							]
-//		    						],
-//		    						vec![ //dirt
-//		    							vec![//top
-//											Vertex{uv: Vector2::new(2.0, 0.0),vertex: Vector3::new(0.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(3.0, 0.0),vertex: Vector3::new(1.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(2.0, 1.0),vertex: Vector3::new(0.0,1.0,1.0)},
-//											
-//											Vertex{uv: Vector2::new(2.0, 1.0),vertex: Vector3::new(0.0,1.0,1.0)},
-//											Vertex{uv: Vector2::new(3.0, 0.0),vertex: Vector3::new(1.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(3.0, 1.0),vertex: Vector3::new(1.0,1.0,1.0)}
-//		    							],
-//		    							
-//		    							vec![//botton
-//		    								Vertex{uv: Vector2::new(2.0, 0.0),vertex: Vector3::new(0.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(3.0, 1.0),vertex: Vector3::new(1.0,0.0,0.0)},
-//		    								Vertex{uv: Vector2::new(2.0, 1.0),vertex: Vector3::new(0.0,0.0,0.0)},
-//		    								
-//		    								Vertex{uv: Vector2::new(3.0, 0.0),vertex: Vector3::new(1.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(3.0, 1.0),vertex: Vector3::new(1.0,0.0,0.0)},
-//		    								Vertex{uv: Vector2::new(2.0, 0.0),vertex: Vector3::new(0.0,0.0,1.0)}
-//		    							],
-//		    							
-//		    							vec![//right
-//		    								Vertex{uv: Vector2::new(3.0, 1.0), vertex: Vector3::new(1.0,0.0,0.0)},
-//		    								Vertex{uv: Vector2::new(2.0, 1.0), vertex: Vector3::new(1.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(3.0, 0.0), vertex: Vector3::new(1.0,1.0,0.0)},
-//		    								
-//		    								Vertex{uv: Vector2::new(2.0, 1.0), vertex: Vector3::new(1.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(2.0, 0.0), vertex: Vector3::new(1.0,1.0,1.0)},
-//		    								Vertex{uv: Vector2::new(3.0, 0.0), vertex: Vector3::new(1.0,1.0,0.0)}
-//		    							],
-//		    							
-//		    							vec![//left
-//		    								Vertex{uv: Vector2::new(2.0, 0.0), vertex: Vector3::new(0.0,1.0,0.0)},
-//		    								Vertex{uv: Vector2::new(3.0, 1.0), vertex: Vector3::new(0.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(2.0, 1.0), vertex: Vector3::new(0.0,0.0,0.0)},
-//		    								
-//		    								Vertex{uv: Vector2::new(2.0, 0.0), vertex: Vector3::new(0.0,1.0,1.0)},
-//		    								Vertex{uv: Vector2::new(2.0, 1.0), vertex: Vector3::new(0.0,0.0,1.0)},
-//		    								Vertex{uv: Vector2::new(3.0, 0.0), vertex: Vector3::new(0.0,1.0,0.0)}
-//		    							],
-//		    							
-//		    							vec![//back
-//											Vertex{uv: Vector2::new(2.0,1.0), vertex: Vector3::new(0.0,0.0,1.0)},
-//											Vertex{uv: Vector2::new(2.0,0.0), vertex: Vector3::new(0.0,1.0,1.0)},
-//											Vertex{uv: Vector2::new(3.0,1.0), vertex: Vector3::new(1.0,0.0,1.0)},
-//											
-//											Vertex{uv: Vector2::new(3.0,1.0), vertex: Vector3::new(1.0,0.0,1.0)},
-//											Vertex{uv: Vector2::new(2.0,0.0), vertex: Vector3::new(0.0,1.0,1.0)},
-//											Vertex{uv: Vector2::new(3.0,0.0), vertex: Vector3::new(1.0,1.0,1.0)},
-//		    							],
-//		    							
-//		    							vec![//front
-//											Vertex{uv: Vector2::new(2.0,1.0), vertex: Vector3::new(1.0,0.0,0.0)},
-//											Vertex{uv: Vector2::new(3.0,0.0), vertex: Vector3::new(0.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(3.0,1.0), vertex: Vector3::new(0.0,0.0,0.0)},
-//											
-//											Vertex{uv: Vector2::new(2.0,0.0), vertex: Vector3::new(1.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(3.0,0.0), vertex: Vector3::new(0.0,1.0,0.0)},
-//											Vertex{uv: Vector2::new(2.0,1.0), vertex: Vector3::new(1.0,0.0,0.0)},
-//		    							]
-//		    						]],
         	chunks_data: vec![vec![vec![1u16;16usize];16usize];16usize],
         	chunk_size: 16u8
         }
@@ -155,25 +32,32 @@ impl HelloWorld {
 
     #[export]
     fn _ready(&mut self, owner: &Spatial) {
-        let st = SurfaceTool::new();
-        let meshinst = MeshInstance::new();
-        
-        godot_print!("Vector3({},{},{})", 2.9f32.floor() % self.chunk_size as f32, 2.9f32.floor() % self.chunk_size as f32, 3.9f32.floor() % self.chunk_size as f32);
-        
-        let noise = OpenSimplexNoise::new();
+		self.create_chunk(owner);
+    }
+    
+    fn create_chunk(&mut self,owner:&Spatial){
+    	let noise = OpenSimplexNoise::new();
         
         for x in 0..self.chunk_size{
         	for y in 0..self.chunk_size{
         		for z in 0..self.chunk_size{
-        			if noise.get_noise_3d(x as f64, y as f64, z as f64) > y as f64{
+        			if noise.get_noise_2d(x as f64, z as f64) > y as f64{
         				self.chunks_data[x as usize][y as usize][z as usize] = 0u16;
 //        				self.chunks_data[x as usize][(y - 1u8) as usize][z as usize] = 2u16;
         			}
         		}
         	}
 		}
+		godot_print!("{:?}",self.chunks_data);
+        let meshinst = MeshInstance::new();
         
-        let grass:Vec<Vec<Vertex>> = vec![vec![//top
+        meshinst.set_mesh(self.create_chunk_mesh());
+        owner.add_child(meshinst,true);
+    }
+    
+    fn create_chunk_mesh(&self) -> gdnative::Ref<ArrayMesh>{
+    	let grass:Vec<Vec<Vertex>> = vec![ //grass
+		    							vec![//top
 											Vertex{uv: Vector2::new(1.0, 0.0),vertex: Vector3::new(0.0,1.0,0.0)},
 											Vertex{uv: Vector2::new(2.0, 0.0),vertex: Vector3::new(1.0,1.0,0.0)},
 											Vertex{uv: Vector2::new(1.0, 1.0),vertex: Vector3::new(0.0,1.0,1.0)},
@@ -233,8 +117,9 @@ impl HelloWorld {
 											Vertex{uv: Vector2::new(0.0,1.0), vertex: Vector3::new(1.0,0.0,0.0)},
 		    							]
 		    						];
-		    						
-		let dirt:Vec<Vec<Vertex>> = vec![vec![//top
+    	
+    	let dirt:Vec<Vec<Vertex>> = vec![ //dirt
+		    							vec![//top
 											Vertex{uv: Vector2::new(2.0, 0.0),vertex: Vector3::new(0.0,1.0,0.0)},
 											Vertex{uv: Vector2::new(3.0, 0.0),vertex: Vector3::new(1.0,1.0,0.0)},
 											Vertex{uv: Vector2::new(2.0, 1.0),vertex: Vector3::new(0.0,1.0,1.0)},
@@ -294,8 +179,10 @@ impl HelloWorld {
 											Vertex{uv: Vector2::new(2.0,1.0), vertex: Vector3::new(1.0,0.0,0.0)},
 		    							]
 		    						];
-        
-        let size:Vec<u8> = vec![4u8,4u8];
+    
+    	let st = SurfaceTool::new();
+    
+    	let size:Vec<u8> = vec![4u8,4u8];
         
 		st.begin(Mesh::PRIMITIVE_TRIANGLES);
 //		st.begin(Mesh::PRIMITIVE_LINES);
@@ -303,13 +190,13 @@ impl HelloWorld {
         for x in 0..self.chunk_size{
         	for y in 0..self.chunk_size{
         		for z in 0..self.chunk_size{
-        			godot_print!("{}",self.get_voxel(Vector3::new(x as f32,y as f32,z as f32)));
+//        			godot_print!("{}",self.get_voxel(Vector3::new(x as f32,y as f32,z as f32)));
 //        			if self.get_voxel(Vector3::new(x as f32,y as f32,z as f32)) == 0u16{ return }
-        			if self.get_voxel(Vector3::new(x as f32,y as f32,z as f32)) == 1u16{
+//        			if self.get_voxel(Vector3::new(x as f32,y as f32,z as f32)) == 1u16{
         				self.create_custom_voxel(&st,Vector3::new(x as f32,y as f32,z as f32),&grass,&size);
-        			}else{
-        				self.create_custom_voxel(&st,Vector3::new(x as f32,y as f32,z as f32),&dirt,&size);
-        			}
+//        			}else{
+//        				self.create_custom_voxel(&st,Vector3::new(x as f32 + pos.x,y as f32 + pos.y,z as f32 + pos.z),&dirt,&size);
+//        			}
         		}
         	}
 		}
@@ -319,14 +206,11 @@ impl HelloWorld {
             GodotString::from_str("Resource"), false).unwrap().cast::<Material>().unwrap());
         st.generate_normals(false);
         
-        meshinst.set_mesh(st.commit(gdnative::Null::null(), Mesh::ARRAY_COMPRESS_DEFAULT).unwrap());
-        owner.add_child(meshinst,true);
+        return st.commit(gdnative::Null::null(), Mesh::ARRAY_COMPRESS_DEFAULT).unwrap()
     }
     
     fn create_custom_voxel(&self,st:&Ref<SurfaceTool, Unique>, pos:Vector3, mesh:&Vec<Vec<Vertex>>, size:&Vec<u8>){
-    	if self.get_voxel(Vector3::new(pos.x,pos.y,pos.z)) == 0u16{
-    		return
-		}
+    	if self.get_voxel(Vector3::new(pos.x,pos.y,pos.z)) == 0u16{ return }
     
 		if self.get_voxel(Vector3::new(pos.x,pos.y + 1.0f32,pos.z)) == 0u16{
 		    for v in &mesh[0]{//top
@@ -370,15 +254,23 @@ impl HelloWorld {
 			}
 		}
 	}
-    fn get_voxel(&self,pos:Vector3) -> u16 {//pos.floor() / self.chunk_size is the chunk    pos.floor() % self.chunk_size is the block position
+    fn get_voxel(&self,pos:Vector3) -> u16 {
 		if pos.x < self.chunk_size as f32 && pos.x > 0.0f32 && pos.y < self.chunk_size as f32 && pos.y > 0.0f32 && pos.z < self.chunk_size as f32 && pos.z > 0.0f32 {
 			self.chunks_data[pos.x as usize][pos.y as usize][pos.z as usize]
 		} else { 0u16 }
 	}
 	
 	#[export]
-	fn set_voxel(&mut self, owner: &Spatial, pos:Vector3){
-		godot_print!("this function does nothing");
+	fn set_voxel(&mut self, owner: &Spatial, pos:Vector3){//pos.floor() / self.chunk_size is the chunk    pos.floor() % self.chunk_size is the block position
+		let cpos_x: f32 = (pos.x / self.chunk_size as f32).floor();
+		let cpos_y: f32 = (pos.y / self.chunk_size as f32).floor();
+		let cpos_z: f32 = (pos.z / self.chunk_size as f32).floor();
+		
+		let bpos_x: f32 = pos.x.floor() % self.chunk_size as f32;
+		let bpos_y: f32 = pos.y.floor() % self.chunk_size as f32;
+		let bpos_z: f32 = pos.z.floor() % self.chunk_size as f32;
+		
+		godot_print!("the chunk position is {}, {}, {}\nthe block position is {}, {}, {}", cpos_x,cpos_y,cpos_z,bpos_x,bpos_y,bpos_z);
 	}
 }
 
--- test.gd
+++ test.gd
@@ -8,7 +8,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_parent().set_voxel(Vector3(0.0,0.0,0.0)) # Replace with function body.
+	get_parent().set_voxel(Vector3(16.0,0.0,0.0)) # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
