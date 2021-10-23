e08ff69a7f53a09e79180de12cf8478b4c9c3621
Victor Randall <victor.randall13@gmail.com>
2021-10-23 14:51:13 -0300

    updated world generation and textures

--- src/VoxelSistem/src/lib.rs
+++ src/VoxelSistem/src/lib.rs
@@ -192,6 +192,72 @@ impl VoxelSistem {
 					name: "Dirt".to_string(),
 					data: Vec::new()
 				},
+				Voxel{
+					mesh: Some(vec![ //dirt
+		    							vec![//top
+											Vertex{uv: Vector2::new(0.0, 3.0), v: Vector3::new(0.0,1.0,0.0)},
+											Vertex{uv: Vector2::new(1.0, 3.0), v: Vector3::new(1.0,1.0,0.0)},
+											Vertex{uv: Vector2::new(0.0, 4.0), v: Vector3::new(0.0,1.0,1.0)},
+											
+											Vertex{uv: Vector2::new(0.0, 4.0), v: Vector3::new(0.0,1.0,1.0)},
+											Vertex{uv: Vector2::new(1.0, 3.0), v: Vector3::new(1.0,1.0,0.0)},
+											Vertex{uv: Vector2::new(1.0, 4.0), v: Vector3::new(1.0,1.0,1.0)}
+		    							],
+		    							
+		    							vec![//botton
+		    								Vertex{uv: Vector2::new(0.0, 3.0), v: Vector3::new(0.0,0.0,1.0)},
+		    								Vertex{uv: Vector2::new(1.0, 4.0), v: Vector3::new(1.0,0.0,0.0)},
+		    								Vertex{uv: Vector2::new(0.0, 4.0), v: Vector3::new(0.0,0.0,0.0)},
+		    								
+		    								Vertex{uv: Vector2::new(1.0, 3.0), v: Vector3::new(1.0,0.0,1.0)},
+		    								Vertex{uv: Vector2::new(1.0, 4.0), v: Vector3::new(1.0,0.0,0.0)},
+		    								Vertex{uv: Vector2::new(0.0, 3.0), v: Vector3::new(0.0,0.0,1.0)}
+		    							],
+		    							
+		    							vec![//right
+		    								Vertex{uv: Vector2::new(1.0, 4.0), v: Vector3::new(1.0,0.0,0.0)},
+		    								Vertex{uv: Vector2::new(0.0, 4.0), v: Vector3::new(1.0,0.0,1.0)},
+		    								Vertex{uv: Vector2::new(1.0, 3.0), v: Vector3::new(1.0,1.0,0.0)},
+		    								
+		    								Vertex{uv: Vector2::new(0.0, 4.0), v: Vector3::new(1.0,0.0,1.0)},
+		    								Vertex{uv: Vector2::new(0.0, 3.0), v: Vector3::new(1.0,1.0,1.0)},
+		    								Vertex{uv: Vector2::new(1.0, 3.0), v: Vector3::new(1.0,1.0,0.0)}
+		    							],
+		    							
+		    							vec![//left
+		    								Vertex{uv: Vector2::new(0.0, 3.0), v: Vector3::new(0.0,1.0,0.0)},
+		    								Vertex{uv: Vector2::new(1.0, 4.0), v: Vector3::new(0.0,0.0,1.0)},
+		    								Vertex{uv: Vector2::new(0.0, 4.0), v: Vector3::new(0.0,0.0,0.0)},
+		    								
+		    								Vertex{uv: Vector2::new(0.0, 3.0), v: Vector3::new(0.0,1.0,1.0)},
+		    								Vertex{uv: Vector2::new(0.0, 4.0), v: Vector3::new(0.0,0.0,1.0)},
+		    								Vertex{uv: Vector2::new(1.0, 3.0), v: Vector3::new(0.0,1.0,0.0)}
+		    							],
+		    							
+		    							vec![//back
+											Vertex{uv: Vector2::new(0.0, 4.0), v: Vector3::new(0.0,0.0,1.0)},
+											Vertex{uv: Vector2::new(0.0, 3.0), v: Vector3::new(0.0,1.0,1.0)},
+											Vertex{uv: Vector2::new(1.0, 4.0), v: Vector3::new(1.0,0.0,1.0)},
+											
+											Vertex{uv: Vector2::new(1.0, 4.0), v: Vector3::new(1.0,0.0,1.0)},
+											Vertex{uv: Vector2::new(0.0, 3.0), v: Vector3::new(0.0,1.0,1.0)},
+											Vertex{uv: Vector2::new(1.0, 3.0), v: Vector3::new(1.0,1.0,1.0)},
+		    							],
+		    							
+		    							vec![//front
+											Vertex{uv: Vector2::new(0.0, 4.0), v: Vector3::new(1.0,0.0,0.0)},
+											Vertex{uv: Vector2::new(1.0, 3.0), v: Vector3::new(0.0,1.0,0.0)},
+											Vertex{uv: Vector2::new(1.0, 4.0), v: Vector3::new(0.0,0.0,0.0)},
+											
+											Vertex{uv: Vector2::new(0.0, 3.0), v: Vector3::new(1.0,1.0,0.0)},
+											Vertex{uv: Vector2::new(1.0, 3.0), v: Vector3::new(0.0,1.0,0.0)},
+											Vertex{uv: Vector2::new(0.0, 4.0), v: Vector3::new(1.0,0.0,0.0)},
+		    							]
+		    						]),
+					solid: false,
+					name: "Stone".to_string(),
+					data: Vec::new()
+				},
 			],
 			chunks: std::collections::HashMap::<ChunkPos, VoxelChunk>::new(),
 			chunk_size: 16u8,
@@ -202,7 +268,13 @@ impl VoxelSistem {
 	#[export]
 	fn _ready(&mut self, owner: &Spatial) {
 		godot_print!("Hello, world.");
-		self.build_chunk(owner, Vector3::new(0f32,0f32,0f32))
+		for x in 0..10{
+			for y in 0..10{
+				for z in 0..10{
+					self.build_chunk(owner, Vector3::new(x as f32 * (self.chunk_size as f32 - 1f32), y as f32 * (self.chunk_size as f32 - 1f32), z as f32 * (self.chunk_size as f32 - 1f32)))
+				}
+			}
+		}
 	}
 	
 	#[export]
@@ -218,7 +290,7 @@ impl VoxelSistem {
 			(pos.z.floor() / self.chunk_size as f32).floor() as i32
 		];
 		
-		let bpos: Vec<f32> = vec![
+		let bpos: Vec<i32> = vec![
 			(pos.x.floor() % self.chunk_size as f32) as i32,
 			(pos.y.floor() % self.chunk_size as f32) as i32,
 			(pos.z.floor() % self.chunk_size as f32) as i32
@@ -227,8 +299,10 @@ impl VoxelSistem {
 		godot_print!("the chunk pos is Vector3({},{},{})\nthe voxel pos is Vector3({},{},{})",
 					cpos[0],cpos[1],cpos[2],  
 					bpos[0],bpos[1],bpos[2]);
-		self.chunks.get_mut(&ChunkPos{x:cpos[0],y:cpos[1],z:cpos[2]}).unwrap().data[bpos[0] as usize][bpos[1] as usize][bpos[2] as usize] = id;
 		
+		godot_print!("{}", self.chunks.get_mut(&ChunkPos{x:cpos[0],y:cpos[1],z:cpos[2]}).unwrap().data[bpos[0] as usize][bpos[1] as usize][bpos[2] as usize]);
+		self.chunks.get_mut(&ChunkPos{x:cpos[0],y:cpos[1],z:cpos[2]}).unwrap().data[bpos[0] as usize][bpos[1] as usize][bpos[2] as usize] = id;
+		godot_print!("{}", self.chunks.get_mut(&ChunkPos{x:cpos[0],y:cpos[1],z:cpos[2]}).unwrap().data[bpos[0] as usize][bpos[1] as usize][bpos[2] as usize]);
 	}
 	
 	fn build_chunk(&mut self, owner: &Spatial, pos: Vector3){
@@ -243,9 +317,17 @@ impl VoxelSistem {
 			for x in 0..self.chunk_size{
 				for y in 0..self.chunk_size{
 					for z in 0..self.chunk_size{
-						if noise.get_noise_3d(x as f64 + pos.x as f64, y as f64 + pos.y as f64, z as f64 + pos.z as f64)*30f64+10f64 > y as f64 + pos.y as f64{
-							chunk.data[x as usize][y as usize][z as usize] = 1u16;
+						if noise.get_noise_3d(x as f64 + pos.x as f64, y as f64 + pos.y as f64, z as f64 + pos.z as f64)*30f64+20f64 > y as f64 + pos.y as f64{
+							chunk.set_voxel(Vector3::new(x as f32, y as f32, z as f32), 1u16);
+							chunk.set_voxel(Vector3::new(x as f32, y as f32 - 1f32, z as f32), 2u16);
+							chunk.set_voxel(Vector3::new(x as f32, y as f32 - 4f32, z as f32), 3u16);
 						}
+//						if noise.get_noise_3d(x as f64 + pos.x as f64, y as f64 + pos.y as f64, z as f64 + pos.z as f64)*30f64+19f64 > y as f64 + pos.y as f64{
+//							chunk.data[x as usize][y as usize][z as usize] = 2u16;
+//						}
+//						if noise.get_noise_3d(x as f64 + pos.x as f64, y as f64 + pos.y as f64, z as f64 + pos.z as f64)*30f64+13f64 > y as f64 + pos.y as f64{
+//							chunk.data[x as usize][y as usize][z as usize] = 3u16;
+//						}
 					}
 				}
 			}
@@ -262,7 +344,7 @@ impl VoxelSistem {
 							&st,
 							Vector3::new(x as f32,y as f32,z as f32),
 							chunk.get_voxel(Vector3::new(x as f32,y as f32,z as f32)),
-							&vec![4u8,4u8]);
+							&vec![8u8,4u8]);
 		    		}
 		    	}
 			}
@@ -270,6 +352,7 @@ impl VoxelSistem {
 		    st.set_material(ResourceLoader::godot_singleton().load(
 		        GodotString::from_str("res://assets/new_spatialmaterial.tres"),
 		        GodotString::from_str("Resource"), false).unwrap().cast::<Material>().unwrap());
+		    st.add_smooth_group(true);
 		    st.generate_normals(false);
 		    
 //			meshinst.set_mesh(&chunk.build_chunk_mesh(ChunkPos{x: pos.x as i32,y: pos.y as i32,z: pos.z as i32}));
@@ -298,7 +381,7 @@ impl VoxelSistem {
 							&st,
 							Vector3::new(x as f32,y as f32,z as f32),
 							chunk.get_voxel(Vector3::new(x as f32,y as f32,z as f32)),
-							&vec![4u8,4u8]);
+							&vec![8u8,4u8]);
 		    		}
 		    	}
 			}
@@ -306,6 +389,7 @@ impl VoxelSistem {
 		    st.set_material(ResourceLoader::godot_singleton().load(
 		        GodotString::from_str("res://assets/new_spatialmaterial.tres"),
 		        GodotString::from_str("Resource"), false).unwrap().cast::<Material>().unwrap());
+		    st.add_smooth_group(true);
 		    st.generate_normals(false);
 		    
 		    unsafe {
@@ -396,8 +480,10 @@ impl VoxelChunk{
 		} else { 0u16 }
 	}
 	
-	pub fn set_voxel(&mut self,x:f32,y:f32,z:f32, id:u16){
-		self.data[x as usize][y as usize][z as usize] = id
+	pub fn set_voxel(&mut self,pos: Vector3, id:u16){
+		if pos.x < self.size as f32 && pos.x > 0.0f32 && pos.y < self.size as f32 && pos.y > 0.0f32 && pos.z < self.size as f32 && pos.z > 0.0f32 {
+			self.data[pos.x as usize][pos.y as usize][pos.z as usize] = id
+		}
 	}
 }
 
